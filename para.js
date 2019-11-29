eb07e8cc91dce47c8ae711c00049b4a786e745f2
Jon Collins <47168633+GoloisaNinja@users.noreply.github.com>
2019-11-29 10:53:31 -0500

    Update para.js

--- para.js
+++ para.js
@@ -1,5 +1,14 @@
-var event = ('ontouchstart' in window) ? 'click' : 'mouseenter mouseleave';
+var event = ('ontouchstart' in window);
+// ? 'click' : 'mouseenter mouseleave';
 
-$('.tile').on(event, function () {
-    $(this).toggleClass('hover');
+// $('.tile').on(event, function () {
+//    $(this).toggleClass('hover');
+// });
+
+var topofDiv = $(".tile").offset().top; //gets offset of header
+$(window).scroll(function(){
+    if($(window).scrollTop() > topofDiv && event){
+       $(this).toggleClass('tile');
+    }
+    
 });

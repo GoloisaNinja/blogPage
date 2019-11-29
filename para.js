a3c466589b2b1385996a629e099fac2ecc00bbe1
Jon Collins <47168633+GoloisaNinja@users.noreply.github.com>
2019-11-29 13:11:36 -0500

    Update para.js

--- para.js
+++ para.js
@@ -1,14 +1,44 @@
 var event = ('ontouchstart' in window);
-// ? 'click' : 'mouseenter mouseleave';
+// ? 'click' : 'hover';
 
-// $('.tile').on(event, function () {
-//    $(this).toggleClass('hover');
+// $('.design-section figure').on(event, function () {
+//     $(this).toggleClass('tile');
 // });
 
-var topofDiv = $(".tile").offset().top; //gets offset of header
-$(window).scroll(function(){
-    if($(window).scrollTop() > topofDiv && event){
-       $(this).toggleClass('tile');
+// var topofDiv = $(".test").offset().top; //gets offset of header
+// $(window).scroll(function(){
+//     if($(window).scrollTop() > topofDiv){
+//        $(".tile").toggleClass('hover');
+//     }
+//
+// });
+
+
+//Cache reference to window and animation items
+var $animation_elements = $('.tile');
+var $window = $(window);
+
+$window.on('scroll', check_if_in_view);
+
+$window.on('scroll resize', check_if_in_view);
+
+$window.trigger('scroll');
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position) && event) {
+      $element.toggleClass('hover');
     }
-    
-});
+  });
+}

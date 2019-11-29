5717f80a55911cd6d015f5654a9df621ecc5a75e
Jon Collins <47168633+GoloisaNinja@users.noreply.github.com>
2019-11-29 14:02:34 -0500

    Update para.js

--- para.js
+++ para.js
@@ -20,13 +20,13 @@ var $window = $(window);
 
 
 function check_if_in_view() {
-  var window_height = $window.height();
+  var window_height = $window.height() / 2
   var window_top_position = $window.scrollTop();
   var window_bottom_position = (window_top_position + window_height);
 
   $.each($animation_elements, function() {
     var $element = $(this);
-    var element_height = $element.outerHeight();
+    var element_height = $element.outerHeight() / 2
     var element_top_position = $element.offset().top;
     var element_bottom_position = (element_top_position + element_height);
 
